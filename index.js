774497a2cbf287c3afa9b2745ad6822906eceb35
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-06-09 15:24:30 +0200

    added comments to js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let cards = document.querySelectorAll(".card-img");
 
 for (let card of cards) {
+    //add an event listener for each card
     card.addEventListener("click", () => {
+        //if the clicked card is active, make it inactive
         if (card.classList.contains("active")) {
             card.classList.remove("active")
         } else {
+        //if the clicked card isn't active, make any other active card inactive and make the clicked card active
             for (let card of cards) {
                 card.classList.remove("active");
             }
